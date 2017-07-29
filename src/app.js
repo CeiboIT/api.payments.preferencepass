1f3d789bca6973216874b77590ef68d1e1e89c8a
JLMosconi <jlmosconi@gmail.com>
2017-07-28 22:21:01 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const { json, send }  = require('micro')
 const { router, post, get } = require('microrouter')
 const payment = require('./payment')
 
+const microCors = require('micro-cors')
+const cors = microCors({ 
+  origin: 'http://localhost:4200'
+})
+
 const postSubscription = async (req, res) => {
     try {
       let body = await json(req)
@@ -18,7 +23,7 @@ const notfound = (req, res) =>
   send(res, 404, 'Could not find route', req.url)
 
 module.exports = router(
-  post('/subscription/new', postSubscription),
+  post('/subscription/new', cors(postSubscription)),
   get('/*', notfound),
   post('/*', notfound)
 )
